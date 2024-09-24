31f9b2cb78f3985e740a92f0f8cb5af0d90035eb
mnycz-ch <167440954+mnycz-ch@users.noreply.github.com>
2024-09-24 10:28:59 +0100

    JU-599 WebApplicationSecurity

--- src/main/java/uk/gov/companieshouse/efs/web/security/WebApplicationSecurity.java
+++ src/main/java/uk/gov/companieshouse/efs/web/security/WebApplicationSecurity.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import uk.gov.companieshouse.auth.filter.CompanyAuthFilter;
 import uk.gov.companieshouse.auth.filter.HijackFilter;
 import uk.gov.companieshouse.auth.filter.UserAuthFilter;
@@ -24,8 +23,13 @@ import uk.gov.companieshouse.session.handler.SessionHandler;
  */
 @EnableWebSecurity
 public class WebApplicationSecurity {
-    @Value("${chs.signout.redirect.path}")
-    private String signoutRedirectPath;
+
+    private final String signoutRedirectPath;
+    private final String startPageUrl;
+    private final String accessibilityStatementPageUrl;
+    private final String guidancePageUrl;
+    private final String insolvencyGuidancePageUrl;
+    private final String serviceUnavailablePageUrl;
     private ApiClientService apiClientService;
     private FormTemplateService formTemplateService;
     private CategoryTemplateService categoryTemplateService;
@@ -34,185 +38,69 @@ public class WebApplicationSecurity {
     /**
      * Constructor.
      *
-     * @param apiClientService              apiClient service
-     * @param formTemplateService           formTemplate service
-     * @param categoryTemplateService       categoryTemplate service
+     * @param apiClientService        apiClient service
+     * @param formTemplateService     formTemplate service
+     * @param categoryTemplateService categoryTemplate service
      */
     @Autowired
-    public WebApplicationSecurity(
-        final ApiClientService apiClientService, FormTemplateService formTemplateService,
-        final CategoryTemplateService categoryTemplateService, final EnvironmentReader environmentReader) {
+    public WebApplicationSecurity (
+            final ApiClientService apiClientService, FormTemplateService formTemplateService,
+            final CategoryTemplateService categoryTemplateService, final EnvironmentReader environmentReader,
+            @Value("${chs.signout.redirect.path}") String signoutRedirectPath,
+            @Value("${start.page.url}") String startPageUrl,
+            @Value("${accessibility.statement.page.url}") String accessibilityStatementPageUrl,
+            @Value("${guidance.page.url}") String guidancePageUrl,
+            @Value("${insolvency.guidance.page.url}") String insolvencyGuidancePageUrl,
+            @Value("${service.unavailable.page.url}") String serviceUnavailablePageUrl) {
         this.apiClientService = apiClientService;
         this.formTemplateService = formTemplateService;
         this.categoryTemplateService = categoryTemplateService;
         this.environmentReader = environmentReader;
+        this.signoutRedirectPath = signoutRedirectPath;
+        this.startPageUrl = startPageUrl;
+        this.accessibilityStatementPageUrl = accessibilityStatementPageUrl;
+        this.guidancePageUrl = guidancePageUrl;
+        this.insolvencyGuidancePageUrl = insolvencyGuidancePageUrl;
+        this.serviceUnavailablePageUrl = serviceUnavailablePageUrl;
     }
 
-    /**
-     * static nested class for root level security.
-     */
-    /**
-     * static nested class for root level security.
-     */
-    @Configuration
     @Order(1)
-    public static class RootLevelSecurityConfig {
-
-        @Bean
-        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-            return http.securityMatcher("/efs-submission").build();
-        }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.securityMatcher("/efs-submission",
+                signoutRedirectPath,
+                startPageUrl,
+                accessibilityStatementPageUrl,
+                guidancePageUrl,
+                insolvencyGuidancePageUrl,
+                serviceUnavailablePageUrl)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/**").permitAll())
+                .build();
     }
-
-    /**
-     * static nested class for start page security.
-     */
-    @Configuration
+    
     @Order(2)
-    public static class StartPageSecurityConfig {
-        private final String startPageUrl;
+    @Bean
+    public SecurityFilterChain companyAuthFilterChain(HttpSecurity http) throws Exception {
 
-        public StartPageSecurityConfig(@Value("${start.page.url}") final String startPageUrl) {
-            this.startPageUrl = startPageUrl;
-        }
-
-        @Bean
-        public SecurityFilterChain startPageSecurityChain(HttpSecurity http) throws Exception {
-            return http.securityMatcher(startPageUrl).build();
-        }
+        return http.securityMatcher("/efs-submission/*/company/**")
+                .addFilterBefore(new SessionHandler(), BasicAuthenticationFilter.class)
+                .addFilterBefore(new HijackFilter(), BasicAuthenticationFilter.class)
+                .addFilterBefore(new UserAuthFilter(), BasicAuthenticationFilter.class)
+                .addFilterBefore(new CompanyAuthFilter(), BasicAuthenticationFilter.class).build();
     }
 
-    /**
-     * static nested class for accessibility statement page security.
-     */
-    @Configuration
-    @Order(3)
-    public static class AccessibilityStatementPageSecurityConfig {
-        private final String accessibilityStatementPageUrl;
-
-        public AccessibilityStatementPageSecurityConfig(
-            @Value("${accessibility.statement.page.url}") final String accessibilityStatementPageUrl) {
-            this.accessibilityStatementPageUrl = accessibilityStatementPageUrl;
-        }
-
-        @Bean
-        public SecurityFilterChain accessibilityStatementPageSecurityChain(HttpSecurity http) throws Exception {
-            return http.securityMatcher(accessibilityStatementPageUrl).build();
-        }
-    }
-
-    /**
-     * static nested class for guidance page security.
-     */
-    @Configuration
-    @Order(4)
-    public static class GuidancePageSecurityConfig {
-        private final String guidancePageUrl;
-
-        public GuidancePageSecurityConfig(@Value("${guidance.page.url}") final String guidancePageUrl) {
-            this.guidancePageUrl = guidancePageUrl;
-        }
-
-        @Bean
-        public SecurityFilterChain guidancePageSecurityChain(HttpSecurity http) throws Exception {
-            return http.securityMatcher(guidancePageUrl).build();
-        }
-    }
-
-    /**
-     * static nested class for insolvency guidance page security.
-     */
-    @Configuration
-    @Order(5)
-    public static class InsolvencyGuidancePageSecurityConfig {
-        private String insolvencyGuidancePageUrl;
-
-        public InsolvencyGuidancePageSecurityConfig(
-            @Value("${insolvency.guidance.page.url}") final String insolvencyGuidancePageUrl) {
-            this.insolvencyGuidancePageUrl = insolvencyGuidancePageUrl;
-        }
-
-        @Bean
-        public SecurityFilterChain insolvencyGuidancePageSecurityChain(HttpSecurity http) throws Exception {
-            return http.securityMatcher(insolvencyGuidancePageUrl).build();
-        }
-    }
-
-    /**
-     * static nested class for service unavailable page security.
-     */
-    @Configuration
-    @Order(6)
-    public static class ServiceUnavailablePageSecurityConfig {
-        private String serviceUnavailablePageUrl;
-
-        public ServiceUnavailablePageSecurityConfig(
-                @Value("${service.unavailable.page.url}") final String serviceUnavailablePageUrl) {
-            this.serviceUnavailablePageUrl = serviceUnavailablePageUrl;
-        }
-
-        @Bean
-        public SecurityFilterChain serviceUnavailablePageSecurityChain(HttpSecurity http) throws Exception {
-            return http.securityMatcher(serviceUnavailablePageUrl).build();
-        }
-    }
-
-    @Configuration
-    @Order(7)
-    public class CompanyAuthFilterSecurityConfig {
-
-        @Bean
-        public SecurityFilterChain companyAuthFilterChain(HttpSecurity http) throws Exception {
-//            final LoggingAuthFilter authFilter = new LoggingAuthFilter(signoutRedirectPath);
-//            final CompanyAuthFilter companyAuthFilter =
-//                new CompanyAuthFilter();
-
-            return http.securityMatcher("/efs-submission/*/company/**")
-                    .addFilterBefore(new SessionHandler(), BasicAuthenticationFilter.class)
-                    .addFilterBefore(new HijackFilter(), BasicAuthenticationFilter.class)
-                    .addFilterBefore(new UserAuthFilter(), BasicAuthenticationFilter.class)
-                    .addFilterBefore(new CompanyAuthFilter(), BasicAuthenticationFilter.class).build();
-        }
-//        @Override
-//        protected void configure(HttpSecurity http) {
-//            final LoggingAuthFilter authFilter = new LoggingAuthFilter(signoutRedirectPath);
-//            final CompanyAuthFilter companyAuthFilter =
-//                new CompanyAuthFilter(environmentReader, apiClientService, formTemplateService,
-//                    categoryTemplateService);
-//
-//            http.antMatcher("/efs-submission/*/company/**")
-//                .addFilterBefore(new SessionHandler(), BasicAuthenticationFilter.class)
-//                .addFilterBefore(new HijackFilter(), BasicAuthenticationFilter.class)
-//                .addFilterBefore(authFilter, BasicAuthenticationFilter.class)
-//                .addFilterBefore(companyAuthFilter, BasicAuthenticationFilter.class);
-//        }
-    }
 
     /**
      * static nested class for resource level security.
      */
-    @Configuration
-    @Order(8)
-    public class EfsWebResourceFilterConfig {
+    @Order(3)
+    @Bean
+    public SecurityFilterChain efsWebResourceFilterChain(HttpSecurity http) throws Exception {
 
-        @Bean
-        public SecurityFilterChain efsWebResourceFilterChain(HttpSecurity http) throws Exception {
-//            final LoggingAuthFilter authFilter = new LoggingAuthFilter(signoutRedirectPath);
-
-            return http.securityMatcher("/efs-submission/**")
-                    .addFilterBefore(new SessionHandler(), BasicAuthenticationFilter.class)
-                    .addFilterBefore(new HijackFilter(), BasicAuthenticationFilter.class)
-                    .addFilterBefore(new UserAuthFilter(), BasicAuthenticationFilter.class).build();
-        }
-
-//        @Override
-//        protected void configure(final HttpSecurity http) {
-//            final LoggingAuthFilter authFilter = new LoggingAuthFilter(signoutRedirectPath);
-//
-//            http.antMatcher("/efs-submission/**")
-//                .addFilterBefore(new SessionHandler(), BasicAuthenticationFilter.class)
-//                .addFilterBefore(new HijackFilter(), BasicAuthenticationFilter.class)
-//                .addFilterBefore(authFilter, BasicAuthenticationFilter.class);
-//        }
+        return http.securityMatcher("/efs-submission/**")
+                .addFilterBefore(new SessionHandler(), BasicAuthenticationFilter.class)
+                .addFilterBefore(new HijackFilter(), BasicAuthenticationFilter.class)
+                .addFilterBefore(new UserAuthFilter(), BasicAuthenticationFilter.class).build();
     }
 }
