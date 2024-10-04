a4a3d8b4af2a32d63626f29717e9f19b2a6f999b
nghessen-ch <nghessen@companieshouse.gov.uk>
2024-10-04 15:00:43 +0100

    increasing the health check grace period

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -42,7 +42,7 @@ module "ecs-service" {
   lb_listener_arn                   = data.aws_lb_listener.service_lb_listener.arn
   lb_listener_rule_priority         = local.lb_listener_rule_priority
   lb_listener_paths                 = local.lb_listener_paths
-  health_check_grace_period_seconds = 240
+  health_check_grace_period_seconds = 360
   healthcheck_healthy_threshold     = "2"
   multilb_setup                   = false
   
