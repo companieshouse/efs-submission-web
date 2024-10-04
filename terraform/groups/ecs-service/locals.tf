c170b91bff8027ed60e43cab2ffa0ce23bb68b42
rhilgenbergbastos-ch <162116954+rhilgenbergbastos-ch@users.noreply.github.com>
2024-10-04 10:54:29 +0100

    Revert "fixing hc path for efs-submission-web"

--- terraform/groups/ecs-service/locals.tf
+++ terraform/groups/ecs-service/locals.tf
@@ -9,7 +9,7 @@ locals {
   kms_alias                  = "alias/${var.aws_profile}/environment-services-kms"
   lb_listener_rule_priority  = 70
   lb_listener_paths          = ["/efs-submission*", "/efs-submission"]
-  healthcheck_path           = "/efs-submission/healthcheck" # healthcheck path for efs-submission-web
+  healthcheck_path           = "/efs-submission-web/healthcheck" # healthcheck path for efs-submission-web
   healthcheck_matcher        = "200"
   vpc_name                   = local.stack_secrets["vpc_name"]
   s3_config_bucket           = data.vault_generic_secret.shared_s3.data["config_bucket_name"]
