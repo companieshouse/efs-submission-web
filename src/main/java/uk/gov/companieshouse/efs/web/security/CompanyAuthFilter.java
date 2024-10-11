ae7629f198ce0356d5d2017586eeb4101e21af64
mnycz-ch <167440954+mnycz-ch@users.noreply.github.com>
2024-10-11 15:59:48 +0100

    JU-599-revert custom company auth

---
+++ src/main/java/uk/gov/companieshouse/efs/web/security/CompanyAuthFilter.java
@@ -0,0 +1,98 @@
+package uk.gov.companieshouse.efs.web.security;
+
+import java.io.IOException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import uk.gov.companieshouse.auth.filter.AuthFilter;
+import uk.gov.companieshouse.efs.web.categorytemplates.service.api.CategoryTemplateService;
+import uk.gov.companieshouse.efs.web.formtemplates.service.api.FormTemplateService;
+import uk.gov.companieshouse.efs.web.security.validator.FormTemplateRequiredValidator;
+import uk.gov.companieshouse.efs.web.security.validator.HttpRequestRequiredValidator;
+import uk.gov.companieshouse.efs.web.security.validator.UserRequiredValidator;
+import uk.gov.companieshouse.efs.web.security.validator.Validator;
+import uk.gov.companieshouse.efs.web.security.validator.ValidatorResourceProvider;
+import uk.gov.companieshouse.efs.web.service.api.ApiClientService;
+import uk.gov.companieshouse.environment.EnvironmentReader;
+import uk.gov.companieshouse.session.Session;
+
+
+/**
+ * CompanyAuthFilter ensures that a request is authorised if it is made to a resource that
+ * requires authentication.
+ */
+public class CompanyAuthFilter extends AuthFilter {
+
+    private final ApiClientService apiClientService;
+
+    private final FormTemplateService formTemplateService;
+
+    private final CategoryTemplateService categoryTemplateService;
+
+    /**
+     * Constructor.
+     *
+     * @param environmentReader       dependency
+     * @param apiClientService        dependency
+     * @param formTemplateService     dependency
+     * @param categoryTemplateService dependency
+     */
+    public CompanyAuthFilter(final EnvironmentReader environmentReader,
+                             final ApiClientService apiClientService,
+                             final FormTemplateService formTemplateService,
+                             CategoryTemplateService categoryTemplateService) {
+        super(environmentReader);
+        this.apiClientService = apiClientService;
+        this.formTemplateService = formTemplateService;
+        this.categoryTemplateService = categoryTemplateService;
+    }
+
+    /**
+     * doFilter does the request validation and, if necessary, redirects the user for authentication
+     * before responding.
+     * <p>
+     * It redirects if:
+     * The request is a get request
+     * and made to an endpoint with a company number and submissionId
+     * and has a form that requires authentication
+     * and the user is not authorised for that form or, in the case of Insolvency,
+     * on the allow list.
+     * <p>
+     * The validation for these conditions is implemented as a chain of responsibility
+     * with a "link" for the request checking, form checking, and finally user checking.
+     *
+     * @param request  the request made to the web
+     * @param response the response to the user
+     * @param chain    the chain of filters for authorisation
+     */
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+
+        ValidatorResourceProvider resourceProvider = new ValidatorResourceProvider(apiClientService,
+                formTemplateService);
+        Validator<HttpServletRequest> requiresAuth = new HttpRequestRequiredValidator(resourceProvider)
+                .setNext(new FormTemplateRequiredValidator(resourceProvider))
+                .setNext(new UserRequiredValidator(resourceProvider, categoryTemplateService));
+
+        if (requiresAuth.validate(httpServletRequest)) {
+            String companyNumber = resourceProvider.getCompanyNumber().orElse("");
+            Session chsSession = resourceProvider.getChsSession().orElse(null);
+
+            redirectForAuth(chsSession,
+                    httpServletRequest,
+                    (HttpServletResponse) response,
+                    companyNumber,
+                    false);
+        }
+
+        chain.doFilter(request, response);
+    }
+
+}
--- src/test/java/uk/gov/companieshouse/efs/web/security/WebApplicationSecurityTest.java
+++ src/test/java/uk/gov/companieshouse/efs/web/security/WebApplicationSecurityTest.java
@@ -15,7 +15,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import com.github.stefanbirkner.systemlambda.Statement;
-import uk.gov.companieshouse.auth.filter.CompanyAuthFilter;
 import uk.gov.companieshouse.auth.filter.HijackFilter;
 import uk.gov.companieshouse.auth.filter.UserAuthFilter;
 import uk.gov.companieshouse.efs.web.categorytemplates.service.api.CategoryTemplateService;
