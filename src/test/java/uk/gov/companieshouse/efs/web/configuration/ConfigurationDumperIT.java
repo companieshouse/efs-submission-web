8cbe404dd2a5d19019c5daeefd496e9436c643e2
mnycz-ch <167440954+mnycz-ch@users.noreply.github.com>
2024-10-11 13:10:04 +0100

    revert configurationDumper change

--- src/test/java/uk/gov/companieshouse/efs/web/configuration/ConfigurationDumperIT.java
+++ src/test/java/uk/gov/companieshouse/efs/web/configuration/ConfigurationDumperIT.java
@@ -52,7 +52,6 @@ class ConfigurationDumperIT {
     }
 
     @Test
-    @Disabled
     void handleContextRefresh() {
         // given
         when(event.getApplicationContext()).thenReturn(context);
