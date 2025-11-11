091fd7a276e45713bcffc5ac5018495b03f77b65
clewis1 <clewis1@companieshouse.gov.uk>
2025-11-11 13:44:44 +0000

    Address sonar issues

--- src/main/java/uk/gov/companieshouse/efs/web/controller/BaseControllerImpl.java
+++ src/main/java/uk/gov/companieshouse/efs/web/controller/BaseControllerImpl.java
@@ -4,7 +4,15 @@ import static org.springframework.http.HttpStatus.Series.CLIENT_ERROR;
 import static org.springframework.http.HttpStatus.Series.SERVER_ERROR;
 
 import java.text.MessageFormat;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
